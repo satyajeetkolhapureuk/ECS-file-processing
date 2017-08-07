bb5cb62bdcea6e275e1979c36a3fabcdd384f715
Satyajeet <satyajeet.kolhapure.uk@gmail.com>
2017-08-07 07:33:30 +0100

    ECS code sample

---
+++ ECS File Processing/ECS File Processing/Service1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS_File_Processing
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            //fileSystemWatcher1.BeginInit();
+            //fileSystemWatcher1.
+        }
+
+        protected override void OnStop()
+        {
+            //fileSystemWatcher1.Dispose();
+        }
+
+        private void fileSystemWatcher1_Created(object sender, System.IO.FileSystemEventArgs e)
+        {
+
+        }
+    }
+}
